91872a2d327a74537677948bc14e80e420ef580d
Janaina <janainasoul@icloud.com>
2023-04-12 11:46:58 -0300

    deletando arquivos que não vai usar e criando componente Banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX - parece html mas não é
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal do Organo"/>
+        </header>
+        
+    )
+}
+
+export default Banner
