75250dda876c2bce1a564b877727679ccb7b5093
Janaina <janainasoul@icloud.com>
2023-04-13 19:01:33 -0300

    colocando a propriedade manioulando o input do usuário

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,12 +3,19 @@ import './CampoTexto.css'
 const CampoTexto = (props) => { //props = propriedade
 
     const placeholderModificada =`${props.placeholder}...` //de forma explicita - aqui ta do lado de fora - + legivel
+    
+    let valor = 'Janaina de Souza'
+
+    const aoDigitado = (evento) =>{
+        valor = evento.target.value
+        console.log(valor)
+    }
     return (
     <div className="campo-texto"> 
         <label>
             {props.label}
         </label>
-        <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+        <input value={valor} onChange= {aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
     </div>
 )
     
