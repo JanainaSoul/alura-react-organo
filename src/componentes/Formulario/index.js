f8ffba117898f52373f46081c6d1d1edae659cf5
Janaina <janainasoul@icloud.com>
2023-04-13 18:10:18 -0300

    criando campo da lista suspensa e treminando a parte visual

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,7 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <CampoTexto label="Time" />
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
