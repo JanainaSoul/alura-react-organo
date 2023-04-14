ec71a0bab729469630e7aa70c48d76691f452965
Janaina <janainasoul@icloud.com>
2023-04-14 20:08:54 -0300

    controlando o estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,21 +1,25 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => { //props = propriedade
 
     const placeholderModificada =`${props.placeholder}...` //de forma explicita - aqui ta do lado de fora - + legivel
     
-    let valor = 'Janaina de Souza'
+    //let valor = 'Janaina de Souza'
+
+    //const [valor, setValor] = useState('Janaina de Souza') //rokie - vai afzer algo
 
     const aoDigitado = (evento) =>{
-        valor = evento.target.value
-        console.log(valor)
+        //setValor(evento.target.value)
+        //console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
     return (
     <div className="campo-texto"> 
         <label>
             {props.label}
         </label>
-        <input value={valor} onChange= {aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+        <input value={props.valor} onChange= {aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
     </div>
 )
     
